3e1753d4b47fb7f95656dcaf4921906dfa8127c4
Mikl <gnc.rocket@gmail.com>
2022-11-13 23:38:45 +0300

    Кнопка
    прописали метод нажатия на кнопку

--- main.py
+++ main.py
@@ -3,17 +3,33 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+
+def add():
+    print("12345")
+
 def app():
     app = QApplication(sys.argv)
     window1 = QMainWindow()
     window2 = QMainWindow()
 
-    window1.setWindowTitle("Простая программа 1")
-    window2.setWindowTitle("Простая программа 2")
+    window1.setWindowTitle("Простая программа")
+    #window2.setWindowTitle("Простая программа 2")
     window1.setGeometry(300, 200, 300, 200)
-    window2.setGeometry(300, 600, 400, 200)
+    #window2.setGeometry(300, 600, 400, 200)
 
-    window2.show()
+    text = QtWidgets.QLabel(window1)
+    text.setText('Просто текст')
+    text.move(100, 50)
+    text.adjustSize()
+
+    btn = QtWidgets.QPushButton(window1)
+    btn.setText('Нажми на меня')
+    btn.move(50, 100)
+    btn.setFixedWidth(200)
+    btn.clicked.connect(add)
+
+
+    #window2.show()
     window1.show()
 
     sys.exit(app.exec_())
